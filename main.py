9a5e7e0b514d181e3c2cd0f360c5a4cfda908b3c
Temirkhan <sm@mail.ru>
2021-10-12 10:07:13 +0300

    ДОБАВЛЕНО: проверка на наличии  необходимых ключей в словаре data; проверка на корректность данных из файла json.

--- main.py
+++ main.py
@@ -2,31 +2,48 @@ import json
 import requests
 import csv
 import os.path
+import re
 
-
-# проверка корректности ввода данных
+# проверка корректности расширения и существования файла
 file_name = input('название файла: ')
 while not os.path.isfile(file_name) and file_name[:-5] != '.json':
     file_name = input('файл не найден или файл не является json, введите еще раз: ')
-
 
 # открытие файла для чтения и обработка в случаии ошибки
 with open(file_name, "r") as read_file:
     try:
         data = json.load(read_file)
-        # формирование adress на основер парсинга json файла и списка files с диркториями которые нужно поситить
-        files = data['files']
-        adress = data['proto'] + '://' + data['host'] + ':' + data['port']
     except json.decoder.JSONDecodeError:
-        print('ОШИБКА В ФАЙЛЕ json')
+        print('ОШИБКА: не правильный синтаксис файла json')
         raise SystemExit(1)
 
+# проверка на наличии необходимых ключей
+keys_dict = ['host', 'port', 'proto', 'files']
+for i in keys_dict:
+    if i not in data:
+        print('ОШИБКА: отсутствует поле ' + i)
+        raise SystemExit(1)
+
+# проверка соответсвии формату(ip, host, port) данныйх из файле json
+if re.match(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', data['host']) is None:
+    print('ОШИБКА: не правильный формат IP-адреса')
+    raise SystemExit(1)
+if re.match(r'http', data['proto']) is None:
+    print('ОШИБКА: не правильный формат протокола')
+    raise SystemExit(1)
+if re.match(r"\d{,5}$", data['port']) is None:
+    print('ОШИБКА: не правильный формат порта')
+    raise SystemExit(1)
 
 
+
+
+# формирование adress на основер парсинга json файла и списка files с диркториями которые нужно поситить
+files = data['files']
+adress = data['proto'] + '://' + data['host'] + ':' + data['port']
 
 # список с словарями, которые содержат необходимые данные  из ответов GET запросов. Заполняется в цикле for
 response_data = []
-
 
 # отправка GET запроса и формирование списка с необходимыми данными
 for i in files:
