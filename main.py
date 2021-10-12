9e04e5c39e28355faae853e4e36d452c0837d617
Temirkhan <sm@mail.ru>
2021-10-12 10:36:48 +0300

    ДОБАВЛЕНО: функции(чтение файла, отправка запросов и запись csv файла); все коментарии и выводы в консоль переведены на английский язык

--- main.py
+++ main.py
@@ -4,81 +4,91 @@ import csv
 import os.path
 import re
 
-# проверка корректности расширения и существования файла
-file_name = input('название файла: ')
-while not os.path.isfile(file_name) and file_name[:-5] != '.json':
-    file_name = input('файл не найден или файл не является json, введите еще раз: ')
 
-# открытие файла для чтения и обработка в случаии ошибки
-with open(file_name, "r") as read_file:
-    try:
-        data = json.load(read_file)
-    except json.decoder.JSONDecodeError:
-        print('ОШИБКА: не правильный синтаксис файла json')
+# function to open json file and check data
+def reading_file():
+    # checking the correctness of the extension and the existence of the file
+    file_name = input('file name: ')
+    while not os.path.isfile(file_name) and file_name[:-5] != '.json':
+        file_name = input('file not found or file is not json, enter again: ')
+
+    # opening a file for reading and processing in case of an error
+    with open(file_name, "r") as read_file:
+        try:
+            data = json.load(read_file)
+        except json.decoder.JSONDecodeError:
+            print('ERROR: json file syntax not correct')
+            raise SystemExit(1)
+
+    # checking for the necessary keys
+    keys_dict = ['host', 'port', 'proto', 'files']
+    for i in keys_dict:
+        if i not in data:
+            print('ERROR: missing field ' + i)
+            raise SystemExit(1)
+
+    # check for compliance with the format (ip, host, port) given from the json file
+    if re.match(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', data['host']) is None:
+        print('ERROR: not correct IP address format')
         raise SystemExit(1)
-
-# проверка на наличии необходимых ключей
-keys_dict = ['host', 'port', 'proto', 'files']
-for i in keys_dict:
-    if i not in data:
-        print('ОШИБКА: отсутствует поле ' + i)
+    if re.match(r'http', data['proto']) is None:
+        print('ERROR: incorrect protocol format')
         raise SystemExit(1)
-
-# проверка соответсвии формату(ip, host, port) данныйх из файле json
-if re.match(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', data['host']) is None:
-    print('ОШИБКА: не правильный формат IP-адреса')
-    raise SystemExit(1)
-if re.match(r'http', data['proto']) is None:
-    print('ОШИБКА: не правильный формат протокола')
-    raise SystemExit(1)
-if re.match(r"\d{,5}$", data['port']) is None:
-    print('ОШИБКА: не правильный формат порта')
-    raise SystemExit(1)
+    if re.match(r"\d{,5}$", data['port']) is None:
+        print('ERROR: not correct port format')
+        raise SystemExit(1)
+    return data
 
 
+# function for sending get requests
+def sending_requests(data):
+    # formation of an adress based on parsing a json file and a list of files with directories that need to be visited
+    files = data['files']
+    adress = data['proto'] + '://' + data['host'] + ':' + data['port']
+
+    # a list with dictionaries that contain the necessary data from the responses of GET requests. Filled in for loop
+    response_data = []
+
+    # sending a GET request and forming a list with the necessary data
+    for i in files:
+        # forming url from parsed data
+        url = adress + i
+        # handle exceptions associated with a get request
+        try:
+            response = requests.get(url = url)
+            list_response = {'request URL': url, 'response code': response.status_code, 'comment': ''}
+            if response.status_code == 301 or response.status_code == 302:
+                list_response['comment'] = response.headers['Location']
+        except requests.exceptions.ConnectTimeout:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'ConnectTimeout'}
+        except requests.exceptions.ConnectionError:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'ConnectionRefused'}
+        except requests.exceptions.InvalidURL:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'InvalidURL'}
+        except requests.exceptions.RequestException:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'RequestException'}
+        except requests.exceptions.TooManyRedirects:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'TooManyRedirects'}
+        except requests.exceptions.InvalidHeader:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'InvalidHeader'}
+        except requests.exceptions.ChunkedEncodingError:
+            list_response = {'request URL': url, 'response code': '', 'comment': 'ChunkedEncodingError'}
+        # Writing a dictionary with data to the response_data list
+        response_data.append(list_response)
+    return response_data
 
 
-# формирование adress на основер парсинга json файла и списка files с диркториями которые нужно поситить
-files = data['files']
-adress = data['proto'] + '://' + data['host'] + ':' + data['port']
+# functions for writing data in csv format
+def file_creation_csv(response_data):
+    # creating a otvet.csv file and writing data there from the response_data list
+    with open('otvet.csv', 'w') as csvfile:
+        fieldnames = ['request URL', 'response code', 'comment']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,  delimiter =';')
 
-# список с словарями, которые содержат необходимые данные  из ответов GET запросов. Заполняется в цикле for
-response_data = []
-
-# отправка GET запроса и формирование списка с необходимыми данными
-for i in files:
-    # формирование url из спарсенных данных
-    url = adress + i
-    # обработка исключений связынных с get запросом
-    try:
-        response = requests.get(url = url)
-        list_response = {'request URL': url, 'response code': response.status_code, 'comment': ''}
-        if response.status_code == 301 or response.status_code == 302:
-            list_response['comment'] = response.headers['Location']
-    except requests.exceptions.ConnectTimeout:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'ConnectTimeout'}
-    except requests.exceptions.ConnectionError:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'ConnectionRefused'}
-    except requests.exceptions.InvalidURL:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'InvalidURL'}
-    except requests.exceptions.RequestException:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'RequestException'}
-    except requests.exceptions.TooManyRedirects:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'TooManyRedirects'}
-    except requests.exceptions.InvalidHeader:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'InvalidHeader'}
-    except requests.exceptions.ChunkedEncodingError:
-        list_response = {'request URL': url, 'response code': '', 'comment': 'ChunkedEncodingError'}
-    # Запись словаря с данными в список  response_data
-    response_data.append(list_response)
+        writer.writeheader()
+        writer.writerows(response_data)
 
 
-
-
-# создание файла otvet.csv и запись туда данных из списка response_data
-with open('otvet.csv', 'w') as csvfile:
-    fieldnames = ['request URL', 'response code', 'comment']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,  delimiter =';')
-
-    writer.writeheader()
-    writer.writerows(response_data)
+main_data = reading_file()
+main_response_data = sending_requests(main_data)
+file_creation_csv(main_response_data)
